ac4abd0eccaf3f995e150f5c901c1cd788681df2
Mehdi Belhaj <mehdi.belhaj@usmba.ac.ma>
2022-03-25 19:19:02 +0100

    :sparkles: : Results displayed (js).

--- README.md
+++ README.md
@@ -8,6 +8,11 @@ In this mini-app i used:
 - **CSS**: `flexbox` to create a responsive, centered card.
 - **API**: [OpenWeatherMap](https://openweathermap.org/) API to get the current weather for any city.
 - **JavaScript**: 
-	- `fetch()` to access APIs.
+	- `fetch()` to access API.
 	- to dynamically modify content on the page & background images.
 - **Unsplash**: to get a random background image `https://source.unsplash.com/1600x900/?landscape` ([Unsplash](https://unsplash.com/)).
+
+New feature :
+- :sparkles: Add Initial Location (city of the current user).
+- :sparkles: Add Unit (F/C).
+- :sparkles: Add Translation (Fr & Ar).
--- src/script.js
+++ src/script.js
@@ -1,7 +1,33 @@
 let weather = {
-  "apiKey": "48cf0ed1169dfdaedcb9d8c17a35bca1",
-  "url": "https://api.openweathermap.org/data/2.5/weather?q="
-    + city
-    + "&units=metric&appid="
-    + apiKey
-}
+  apiKey: "48cf0ed1169dfdaedcb9d8c17a35bca1",
+
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apiKey
+    )
+      .then((res) => res.json())
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    /* Extract informations from the data */
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    console.log(name, temp, icon, description, humidity, speed);
+
+    /* Display informations on the page (HTML) */
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".temp").innerText = temp + " °C";
+    document.querySelector(".weather-icon").src =
+      "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".humidity").innerText =
+      "humidity: " + humidity + "%";
+    document.querySelector(".wind-speed").innerText =
+      "Wind speed: " + speed + " km/h";
+  },
+};
