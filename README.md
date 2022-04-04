dcf17d55289381f2f0a956c8562cf9c2a5da1d9c
Mehdi Belhaj <mehdi.belhaj@usmba.ac.ma>
2022-04-04 10:20:51 +0000

    :sparkles: : User's current location Added.

--- README.md
+++ README.md
@@ -6,7 +6,9 @@ This is a simple weather app created using HTML, CSS and JavaScript.
 
 ### In this mini-app i used:
 - **CSS**: `flexbox` to create a responsive, centered card.
-- **API**: [OpenWeatherMap](https://openweathermap.org/) API to get the current weather for any city.
+- **API**:
+	- [OpenWeatherMap](https://openweathermap.org/) API to get the current weather for any city.
+	- [OpenCage Geocoding](https://opencagedata.com/) API to convert coordinates to and from places.
 - **JavaScript**: 
 	- `fetch()` to access API.
 	- to dynamically modify content on the page & background images.
@@ -17,8 +19,12 @@ You can try this app by visiting the following link:
 [https://mehdi-belhaj.github.io/weather-app/src/](https://mehdi-belhaj.github.io/weather-app/src/)
 
 ### New feature Comming:
+- [x] Initial Location (city of the current user).
+- [ ] Display datetime of last check.
 - [ ] Cities name autocomplete.
-- [ ] Initial Location (city of the current user).
 - [ ] Current time (based on user time zone).
 - [ ] Change unit (F/C).
 - [ ] Translation (Fr & Ar).
+
+### Issues:
+- [ ] Fix the bug when the user enters a city name that doesn't exist.
--- src/script.js
+++ src/script.js
@@ -1,23 +1,94 @@
+let user = {
+  default_city: "London",
+
+  api_key: "e1cfb0d8192f42ac8dc7eeb206aa36ee",
+  api_url: "https://api.opencagedata.com/geocode/v1/json",
+
+  reverseGeocode: function (lat, lng) {
+    let request = new XMLHttpRequest();
+
+    request.open("GET", this.generate_api_url(lat, lng), true);
+    request.onload = function () {
+      if (request.status === 200) {
+        // Success!
+        let data = JSON.parse(request.responseText);
+        let city = data.results[0].components.city;
+        weather.fetchWeather(city); // Display user's current city weather
+        console.log("User Location:", data.results[0]);
+      } else if (request.status <= 500) {
+        // We reached our target server, but it returned an error
+
+        console.log("unable to geocode! Response code: " + request.status);
+        var data = JSON.parse(request.responseText);
+        console.log("error msg: " + data.status.message);
+      } else {
+        console.log("server error");
+      }
+    };
+
+    request.onerror = function () {
+      // There was a connection error of some sort
+      console.log("unable to connect to server");
+    };
+
+    request.send(); // make the request
+  },
+
+  getLocation: function () {
+    if (navigator.geolocation) {
+      // navigator support geolocation
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          let lat = position.coords.latitude;
+          let lng = position.coords.longitude;
+          this.reverseGeocode(lat, lng);
+        },
+        (error) => {
+          console.error("navigator.geolocation == false", error);
+          // display default city weather infos
+          weather.fetchWeather(user.default_city);
+        }
+      );
+    } else {
+      // navigator doesn't support geolocation
+      console.log("Geolocation is not supported by this browser.");
+      // display default city weather infos
+      weather.fetchWeather(user.default_city);
+    }
+  },
+
+  generate_api_url: function (latitude, longitude) {
+    return (
+      this.api_url +
+      `?key=${this.api_key}` +
+      `&q=${encodeURIComponent(latitude + "," + longitude)}` +
+      `&pretty=1&no_annotations=1`
+    );
+  },
+};
+
 let weather = {
-  apiKey: "48cf0ed1169dfdaedcb9d8c17a35bca1",
+  api_key: "48cf0ed1169dfdaedcb9d8c17a35bca1",
+  api_url: "https://api.openweathermap.org/data/2.5/weather?q=",
+
+  generate_api_url: function (city) {
+    return this.api_url + city + "&units=metric&appid=" + this.api_key;
+  },
 
   fetchWeather: function (city) {
-    fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=" +
-        city +
-        "&units=metric&appid=" +
-        this.apiKey
-    )
+    let request_url = this.generate_api_url(city);
+    fetch(request_url)
       .then((res) => res.json())
-      .then((data) => this.displayWeather(data));
+      .then((data) => this.displayWeather(data), (error) => console.log('error'));
   },
+
   displayWeather: function (data) {
     /* Extract informations from the data */
     const { name } = data;
     const { icon, description } = data.weather[0];
     const { temp, humidity } = data.main;
     const { speed } = data.wind;
-    console.log(name, temp, icon, description, humidity, speed);
+    console.log("Weather Information:", data);
 
     /* Display informations on the page (HTML) */
     document.querySelector(".city").innerText = "Weather in " + name;
@@ -31,31 +102,35 @@ let weather = {
       "Wind speed: " + speed + " km/h";
     document.body.style.backgroundImage =
       "url('https://source.unsplash.com/1600x900/?" + name + "')";
-    document.querySelector(".weather").classList.remove("loading")
+    document.querySelector(".weather").classList.toggle("loading");
   },
+
   search: function () {
-    document.querySelector(".weather").classList.add("loading")
+    document.querySelector(".weather").classList.toggle("loading");
     const city = document.querySelector("#search-input").value;
     this.fetchWeather(city);
   },
 };
 
+/** Search bar content  */
+let searchBar = document.querySelector("#search-input");
+let searchBtn = document.querySelector("#search-btn");
+
 /* Clear search input */
-// document.querySelector("#search-input").value = '';
+// searchBar.value = '';
 
-/* input keyUup `Enter` event */
-document
-  .querySelector("#search-input")
-  .addEventListener("keyup", function (event) {
-    if (event.key == "Enter") {
-      weather.search();
-    }
-  });
-
-/* button click event */
-document.querySelector("#search-btn").addEventListener("click", function () {
-  weather.search();
+/* Enter keyup event */
+searchBar.addEventListener("keyup", function (event) {
+  if (event.key == "Enter")
+    if (searchBar.value != "") weather.search();
+    else alert("Please enter a city name!");
 });
 
-/* Default city loaded in the frist time. (will be replaced by user location) */
-weather.fetchWeather("Fes");
+/* Button click event */
+searchBtn.addEventListener("click", function () {
+  if (searchBar.value != "") weather.search();
+  else alert("Please enter a city name!");
+});
+
+/* Loaded the user's current city weather */
+user.getLocation();
